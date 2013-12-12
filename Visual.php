665eab6653dfcb2ff0f298bd973bce070dc078ee
maparrar <maparrar@gmail.com>
2013-12-12 02:00:54 -0500

    Inclusión de la versión wn MediaWiki

--- Visual.php
+++ Visual.php
@@ -1,7 +1,34 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Inicialización de la extensión Visual
+ *
+ * Documentation:   https://github.com/maparrar/Visual
+ * Support:         https://github.com/maparrar/Visual
+ * Source code:     https://github.com/maparrar/Visual
+ *
+ * @file Visual.php
+ * @ingroup Visual
+ *
+ * @licence GNU GPL v2+
+ *
+ * @author Alejandro Parra < maparrar@gmail.com >
  */
+
+/**
+ * This documentation group collects source code files belonging to Visual.
+ *
+ * @defgroup Visual Visual
+ */
+
+define( 'Visual_VERSION', 'visual = 0.1' );
+
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'Visual',
+	'version' => Visual_VERSION,
+	'author' => array( '[https://github.com/maparrar maparrar]' ),
+	'url' => 'https://github.com/maparrar/Visual',
+	'descriptionmsg' => 'visual-desc',
+);
+
+$wgExtensionMessagesFiles['Visual'] = dirname( __FILE__ ) . '/Visual.i18n.php';
